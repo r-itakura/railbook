a5a38fc68ee259b61af2746de8808a8f4e601987
ryo itakura <itakura@openlogi.com>
2017-11-08 14:36:42 +0900

    model findメソッド

---
+++ app/controllers/recordd_controller.rb
@@ -0,0 +1,12 @@
+class RecorddController < ApplicationController
+  def find
+    @books = Book.find([2, 5, 10])
+    logger.debug @books.inspect
+    render 'hello/list'
+  end
+
+  def find_by
+      @book = Book.find_by(publish: '技術評論社')
+      render 'books/show'
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  resources :fan_comments
+  resources :reviews
+  resources :authors
+  resources :users
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'hello/index', to: 'hello#index'
   get 'hello/view'
   get 'hello/list'
+  get 'record/find', to: 'recordd#find'
+  get 'record/find_by', to: 'recordd#find_by'
 end
